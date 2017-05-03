383b3baf94dfeea1476c8418c5081ea698b64d94
Chris Foster <fosterchris22@gmail.com>
2017-05-03 12:05:18 -0400

    Markers must have lat/lng

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,4 +1,10 @@
 class Marker < ActiveRecord::Base
 	validates_uniqueness_of :longitude
 	validates_uniqueness_of :latitude
+
+	validates_presence_of :longitude
+	validates_presence_of :latitude
+	validates_presence_of :name
+	validates_presence_of :yelp_id
+	validates_presence_of :marker_type
 end
