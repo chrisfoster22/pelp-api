98ad2284e1d6f74a533ded7080eb681286774774
Chris Foster <fosterchris22@gmail.com>
2017-05-31 07:56:35 -0400

    one more cors step.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,13 @@ module MapApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
+      end
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
